fba0e346381dbc53e24e74601c7f4dbb0341e4de
Sarah Whalley <s.whalley1130@gmail.com>
2023-01-29 12:03:41 -0700

    Allows 1 or 2 players for rock paper scissors

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 document.addEventListener("DOMContentLoaded", function() 
 { 
     let btn=document.getElementById('RPSbutton');
-    btn.addEventListener('click', playOnePlayerFullGame);
-    let testing=document.getElementById("RPScontent");
-    
-    
+    btn.addEventListener('click', determineNumPlayers);
+    let rpsresult=document.getElementById("RPSresult");
+   
+   
     function input()
     {
         const input=prompt("Rock, paper or scissors?");
@@ -36,18 +36,18 @@ document.addEventListener("DOMContentLoaded", function()
 
         if (comp===player)
         {
-            console.log(`It's a tie! You both chose ${comp}.`);
+            window.alert(`It's a tie! You both chose ${comp}. `);
             return 1;
         }
         else if ((comp==="rock"&&player==="paper")||(comp==="paper"&&player==="scissors")||(comp==="scissors"&&player==="rock"))
         {
-            console.log(`Player wins! PLayer picked ${player} and computer chose ${comp}.`);
+            window.alert(`Player wins! Player picked ${player} and computer chose ${comp}.`);
             return 2;
         }
         else if ((player==="rock"&&comp==="paper")||(player==="paper"&&comp==="scissors")||(player==="scissors"&&comp==="rock"))
         {
-            console.log(`Computer wins! Computer picked ${comp} and player picked ${player}.`);
-            return 3;  
+           window.alert(`Computer wins! Computer picked ${comp} and player picked ${player}.`);
+           return 3;  
         }
     }
 
@@ -79,17 +79,97 @@ document.addEventListener("DOMContentLoaded", function()
 
         if (computerScore===userScore)
         {
-            console.log("This round results in a tie!");
+            rpsresult.innerText="This round results in a tie!";
         }
         else if (computerScore>userScore)
         {
-            console.log(`The computer won with a score of ${computerScore}, and player only scored ${userScore}.`);
+            rpsresult.innerText=`The computer won with a score of ${computerScore}, and player scored ${userScore}.`;
         }
         else 
         {
-            console.log(`The player won with a score of ${userScore}, and computer only scored ${computerScore}.`)
+            rpsresult.innerText=`The player won with a score of ${userScore}, and computer scored ${computerScore}.`;
         }
     }
+
+    function playTwoPlayerRound()
+    {
+        window.alert("Player 1's turn.");
+        let player1=input();
+        window.alert("Player 2's turn.");
+        let player2=input();
+
+        if (player2===player1)
+        {
+            window.alert(`It's a tie! You both chose ${player1}.`);
+            return 1;
+        }
+        else if ((player1==="rock"&&player2==="paper")||(player1==="paper"&&player2==="scissors")||(player1==="scissors"&&player2==="rock"))
+        {
+            window.alert(`Player 2 wins! Player 2 picked ${player2} and player 1 chose ${player1}.`);
+            return 2;
+        }
+        else if ((player2==="rock"&&player1==="paper")||(player2==="paper"&&player1==="scissors")||(player2==="scissors"&&player1==="rock"))
+        {
+           window.alert(`Player 1 wins! Player 1 picked ${player1} and player 2 picked ${player2}.`);
+           return 3;  
+        }
+    }
+
+    function playTwoPlayerFullGame()
+    {
+        let player1Score=0;
+        let player2Score=0;
+
+        for (var i=0;i<5;i++)
+        {
+        const woohoo=playTwoPlayerRound();
+        if (woohoo===1)
+        {
+                console.log("");
+        }
+        else if (woohoo===2)
+        {
+                player2Score++;
+        }
+        else if (woohoo===3)
+        {
+                player1Score++;
+        }
+        else
+        {
+                console.log("Something went wrong");
+        }
+        }
+
+        if (player1Score===player2Score)
+        {
+            rpsresult.innerText="This round results in a tie!";
+        }
+        else if (player1Score>player2Score)
+        {
+            rpsresult.innerText=`Player 1 wins with a score of ${player1Score}, and player 2 scored ${player2Score}.`;
+        }
+        else 
+        {
+            rpsresult.innerText=`The player 2 won with a score of ${player2Score}, and player 1 scored ${player1Score}.`;
+        }
+    }
+
+
+    function determineNumPlayers()
+    {
+        const something = prompt("Do you want to play against someone else? y/n");
+        if (something ==="y")
+        {
+            playTwoPlayerFullGame();
+        }
+        else if (something==="n")
+        {
+            playOnePlayerFullGame();
+        }
+    }
+
+
 });
 
 
